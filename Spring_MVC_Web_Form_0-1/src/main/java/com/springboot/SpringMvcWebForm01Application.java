686d5faf1f952317f16d4f61f727aa0c8df8a8ec
abhi7 <abhi7@DESKTOP-K3NKE37>
2025-07-20 01:25:22 +0530

    First commit

---
+++ Spring_MVC_Web_Form_0-1/src/main/java/com/springboot/SpringMvcWebForm01Application.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebForm01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebForm01Application.class, args);
+	}
+
+}
